cde25b2a21a8015014ac655c328274b4f74d8af0
yanglepin <1440998356@qq.com>
2019-05-24 18:26:57 +0800

    vue头部组件header

---
+++ src/components/myHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="myHeader">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  // 组件标签的名字
+  name: 'my-header',
+  props: {
+    title: String
+  }
+}
+</script>
+<style>
+.myHeader{
+  height: 0.9rem;
+  line-height: 0.9rem;
+  background-color: #b70419;
+}
+h1{
+  margin: 0;
+  font-size: 0.35rem;
+  color: #ffffff;
+}
+</style>
